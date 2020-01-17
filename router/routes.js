fdd66ebcd9295859f3fb58f742456476229eadd7
ksheyon123 <kshyeon123@gmail.com>
2020-01-17 16:24:24 +0900

    RoomMaking

--- router/routes.js
+++ router/routes.js
@@ -5,4 +5,23 @@ router.get('/', (req, res) => {
     res.render('index');
 });
 
+router.post('/room/:id', async (req, res) => {
+    try {
+        console.log('hi')
+        console.log(req.params.id);
+        console.log(req.body)
+        res.status(200).send(req.body)
+    } catch (err) {
+
+    }
+});
+
+router.get('/room/:id', async (req, res) => {
+    try {
+        console.log('get')
+    } catch (err) {
+
+    }
+});
+
 module.exports = router;
--- server.js
+++ server.js
@@ -5,6 +5,15 @@ var path = require('path');
 var static = require('serve-static');
 var socketio = require('socket.io');
 var cors = require('cors');
+var session = require('express-session');
+
+//session
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: true }
+}));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -25,5 +34,34 @@ var server = app.listen(3000, '192.168.0.40', () => {
 var io = socketio.listen(server);
 
 io.sockets.on('connection', (socket) => {
-    console.log('Connect to Socket: ', socket);
+    console.log('Connect to Socket: ', socket.request.connection._peername);
+    socket.remoteAddress = socket.request.connection._peername.address;
+    socket.remotePort = socket.request.connection._peername.port;
+
+    socket.on('message', (message) => {
+        console.log('이벤트를 받았습니다.');
+        console.dir(message);
+
+        if (message.recepient == 'ALL') {
+            io.sockets.emit('message', message);
+        }
+    });
+
+    socket.on('room', (room) => {
+        console.log('방 생성 이벤트를 받았습니다.');
+        console.dir(room);
+        console.log(session)
+        if (room.command == 'create') {
+            if (io.sockets.adapter.rooms[room.roomId]) {
+                console.log('방이 이미 만들어져 있습니다. ');
+            } else {
+                console.log('방을 새로 만듭니다. ');
+
+                socket.join(room.roomId);
+
+                var curRoom = io.sockets.adapter.rooms[room.roomId];
+                console.log('curRoom', curRoom);
+            }
+        }
+    });
 });
